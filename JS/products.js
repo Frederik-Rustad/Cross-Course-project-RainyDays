85f33e877aad2f7d7df8e9bdee81af819e1839bb
Frederik-Rustad <frederik.rustad.89@gmail.com>
2023-04-21 15:02:20 +0200

    images jacket page update
    updated the all jackets page to use the images from the API.
    + is responsive across all screen sizes

--- JS/products.js
+++ JS/products.js
@@ -1,53 +1,64 @@
 // Fetch the API
 fetch('https://api.noroff.dev/api/v1/rainy-days')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
-    // Get the categoryA items
+
+    // Getting the categories as items. (.cattext = Jackets Name, Description and Price)
     const categoryAItems = document.querySelectorAll('#categoryA .cattext');
-    // Loop through each item
+    const categoryBItems = document.querySelectorAll('#categoryB .cattext');
+    const categoryCItems = document.querySelectorAll('#categoryC .cattext');
+
+    // Loop through each item in category A
     categoryAItems.forEach((item, index) => {
       // Get the title, price, and description from the API
-      const title = data[index].title.slice(10); // <- remove the first 10 characters (Rainy Days) ;
+      const title = data[index].title.slice(10); // <- removes the first 10 characters (Rainy Days) ;
       const price = data[index].price;
       const description = data[index].description;
-      // Update the HTML with the title, price, and description
+
+      // Get the image URL from the API
+      const imageURL = data[index].image;
+
+      // Update the HTML with the title, price, description, and image
       item.querySelector('h2').textContent = title;
       item.querySelector('.jacketPrice').textContent = `${price}$`;
       item.querySelector('.description').textContent = description;
+      item.querySelector('.itemlist').setAttribute('src', imageURL);
     });
-
-    // Get the categoryB items
-    const categoryBItems = document.querySelectorAll('#categoryB .cattext');
-    // Loop through each item
+   
+    // Loop through each item in category B
     categoryBItems.forEach((item, index) => {
       // Get the title, price, and description from the API
       const title = data[index + 3].title.slice(10);
       const price = data[index + 3].price;
       const description = data[index + 3].description;
-      // Update the HTML with the title, price, and description
-      item.querySelector('h2').textContent = title;
+
+      // Get the image URL from the API
+      const imageURL = data[index + 3].image;
+
+      // Update the HTML with the title, price, description, and image
+      item.querySelector('h2').textContent = `${title}`;
       item.querySelectorAll('h2')[1].textContent = `${price}$`;
       item.querySelector('.description').textContent = description;
+      item.querySelector('.itemlist').setAttribute('src', imageURL);
     });
-
-    // Get the categoryC items
-    const categoryCItems = document.querySelectorAll('#categoryC .cattext');
-    // Loop through each item
+    
+    // Loop through each item in category C
     categoryCItems.forEach((item, index) => {
       // Get the title, price, and description from the API
       const title = data[index + 6].title.slice(10);
       const price = data[index + 6].price;
       const description = data[index + 6].description;
-      // Update the HTML with the title, price, and description
+
+      // Get the image URL from the API
+      const imageURL = data[index + 6].image;
+
+      // Update the HTML with the title, price, description, and image
       item.querySelector('h2').textContent = title;
       item.querySelectorAll('h2')[1].textContent = `${price}$`;
       item.querySelector('.description').textContent = description;
+      item.querySelector('.itemlist').setAttribute('src', imageURL);
     });
   })
   .catch(error => console.log(error));
 
-
-
 export default [];
-
-
