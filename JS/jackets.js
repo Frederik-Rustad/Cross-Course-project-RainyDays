2f11585a7194dae7738ade4051dee09c412574cd
Frederik-Rustad <frederik.rustad.89@gmail.com>
2023-08-25 11:55:41 +0200

    featured jackets
    filtered featured jackets into their own div

--- JS/jackets.js
+++ JS/jackets.js
@@ -1,35 +1,56 @@
 import { addToCart, renderCartQuantity } from './cart.js';
 
-console.log('Loading jackets.js');
 const productsURL = 'https://freddev.no/wp-json/wc/v3/products';
 const keys = '?consumer_key=ck_30e8103f197d7cd1aa762d83de509977404484c5&consumer_secret=cs_7b79eaa839f2630192c85e81897ff47b8d826eee';
 
 export const productListURL = productsURL + keys;
 
-document.querySelector('.js-products-grid').innerHTML = '<p>Loading...</p>';
+const productsGridContainer = document.querySelector('.js-products-grid');
+const featuredProductsContainer = document.querySelector('.js-featured-products');
+
+productsGridContainer.innerHTML = '<p>Loading...</p>';
 
 fetch(productListURL)
   .then(response => response.json())
   .then(data => {
     let products1HTML = '';
+    let featuredProductsHTML = '';
 
     data.forEach(product => {
       products1HTML += `
         <div class='cattext'>
-        <a class='JacketsImg js-product-detail' data-product-id="${product.id}" href='productDetailPage.html'><img src="${product.images[0].src}" alt='Product' class='itemlist js-product-detail' data-product-id="${product.id}"></a>
-        <h2>${product.name}</h2>
-        <div class='description'>${product.description}</div>
+          <a class='JacketsImg js-product-detail' data-product-id="${product.id}" href='productDetailPage.html'>
+            <img src="${product.images[0].src}" alt='Product' class='itemlist js-product-detail' data-product-id="${product.id}">
+          </a>
+          <h2>${product.name}</h2>
+          <div class='description'>${product.description}</div>
           <h2 class='jacketPrice'> $ ${(product.price)}</h2> 
-          <button class="add-to-cart-button js-add-to-cart"
-          data-product-id="${product.id}">Add to cart</button>
+          <button class="add-to-cart-button js-add-to-cart" data-product-id="${product.id}">Add to cart</button>
         </div> 
       `;
+
+      if (product.featured === true) {
+        featuredProductsHTML += `
+          <div class='featured-product'>
+            <a class='JacketsImg js-product-detail' data-product-id="${product.id}" href='productDetailPage.html'>
+              <img src="${product.images[0].src}" alt='Product' class='itemlist js-product-detail' data-product-id="${product.id}">
+            </a>
+            <h2>${product.name}</h2>
+            <div class='description'>${product.description}</div>
+            <h2 class='jacketPrice'> $ ${(product.price)}</h2> 
+            <button class="add-to-cart-button js-add-to-cart" data-product-id="${product.id}">Add to cart</button>
+          </div>
+        `;
+      }
     });
-    document.querySelector('.js-products-grid').innerHTML = products1HTML;
+
+    productsGridContainer.innerHTML = products1HTML;
+    featuredProductsContainer.innerHTML = featuredProductsHTML;
   })
   .catch(error => {
     console.error('Error fetching products, Refresh the page:', error);
   });
+
 
   document.querySelector('.js-products-grid').addEventListener('click', (event) => {
     if (event.target.classList.contains('js-add-to-cart')) {
@@ -54,5 +75,3 @@ fetch(productListURL)
   }
   
   getIdToDetailsPage();
-  
-  
